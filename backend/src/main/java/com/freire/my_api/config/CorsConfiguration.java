52f340107cada38dcc9405823818275bf6ccf139
LucaFreire <lucasrodifreire@gmail.com>
2023-11-10 16:51:56 -0300

    add: Cors

---
+++ backend/src/main/java/com/freire/my_api/config/CorsConfiguration.java
@@ -0,0 +1,22 @@
+package com.freire.my_api.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                // .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("*")
+                // .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE",
+                // "CONNECT");
+                .allowedMethods("*")
+                .allowedHeaders("*");
+
+    }
+}
