0cfdfa9c5c042e25d9b735655802256d62972589
LucaFreire <lucasrodifreire@gmail.com>
2023-11-13 13:59:49 -0300

    add: login new style

--- frontend/src/pages/loginPage/style.js
+++ frontend/src/pages/loginPage/style.js
@@ -8,7 +8,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         width: "100%",
         height: "100%",
-        backgroundColor: '#E5E5E5'
+        gap: '10px',
+        backgroundColor: '#251336'
     },
     input: {
         padding: "8px",
@@ -20,7 +21,8 @@ const styles = StyleSheet.create({
     },
     label: {
         paddingTop: "15px",
-        right: '100px'
+        right: '100px',
+        color: "white"
     },
     button: {
         alignItems: 'center',
@@ -49,12 +51,17 @@ const styles = StyleSheet.create({
         color: 'white',
     },
     linkButton: {
+        paddingLeft: '8px',
         width: 'auto',
-        color: "purple"
+        color: "purple",
+        alignItems: 'center',
+        justifyContent: 'center'
     },
     headerContent: {
+        paddingTop: "30px",
         display: "flex",
-        paddingTop: '10px'
+        justifyContent: "center",
+        gap: "70px"
     }
 
 });
