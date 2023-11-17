b9eaccfd8064cd3d1414325256d31130bf748993
LucaFreire <lucasrodifreire@gmail.com>
2023-11-17 14:32:55 -0300

    add: historicPage's navigation

--- frontend/src/pages/historicPage/index.js
+++ frontend/src/pages/historicPage/index.js
@@ -1,4 +1,4 @@
-import { TextInput, TouchableOpacity, View, Text, Pressable } from "react-native"
+import { TextInput, TouchableOpacity, View, Text, Pressable, Image, ScrollView } from "react-native"
 import { useCallback, useEffect, useState } from "react"
 import styles from "./style"
 import HistoricCard from "../../components/historicCard"
@@ -6,14 +6,21 @@ import HistoricCard from "../../components/historicCard"
 export default function historicPage(props) {
 
     return (
-        <View style={styles.main}>
-            <HistoricCard />
-            <HistoricCard />
-            <HistoricCard />
-            <HistoricCard />
-            <HistoricCard />
-            <HistoricCard />
-
-        </View>
+        <>
+            <ScrollView style={styles.main}>
+                <TouchableOpacity onPress={() => props.navigation.navigate('home')}>
+                    <Image source={require("../../../assets/arrow.png")} style={styles.arrowStyle} />
+                </TouchableOpacity>
+                <View style={styles.container}>
+                    <h3>Your Historic</h3>
+                    <HistoricCard />
+                    <HistoricCard />
+                    <HistoricCard />
+                    <HistoricCard />
+                    <HistoricCard />
+                    <HistoricCard />
+                </View>
+            </ScrollView>
+        </>
     )
 }
--- frontend/src/pages/historicPage/style.js
+++ frontend/src/pages/historicPage/style.js
@@ -4,8 +4,21 @@ const styles = StyleSheet.create({
     main: {
         flex: 1,
         height: "100%",
-        alignItems: "center",
         backgroundColor: '#251336'
+    },
+    container: {
+        flex: 1,
+        height: "100%",
+        alignItems: "center",
+        gap: "10px",
+        backgroundColor: '#251336'
+    },
+    arrowStyle: {
+        height: "15px",
+        width: "10px",
+        padding: "18px",
+        left: '20px',
+        top: '5px'
     }
 });
 
